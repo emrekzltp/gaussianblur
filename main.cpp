9e6a563d513a883b5c82dd60237123097a955e38
emrekzltp <yunusekzltp@gmail.com>
2025-02-14 22:48:36 +0100

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ std::vector<std::vector<Pixel>> readPPM(const std::string& filename, int& width,
     if (header != "P6") {
         throw std::runtime_error("Invalid PPM file format");
     }
+    
     file >> width >> height;
     int maxVal;
     file >> maxVal;
